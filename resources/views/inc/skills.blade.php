7943b4610ee996f58ee1f582f53602a9ff8ee97d
maahjoub <am470593@gmail.com>
2023-02-22 19:26:34 +0200

    update user inter face

--- resources/views/inc/skills.blade.php
+++ resources/views/inc/skills.blade.php
@@ -1,51 +1,59 @@
 <!-- start team -->
 <section id="team">
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h2 class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s"><span>AWESOME</span> TEAM</h2>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.3s">
-            <div class="team-wrapper">
-                <img src="images/member-1.jpg" class="img-responsive" alt="team img 1">
-                    <div class="team-des">
-                        <h4>Giri Dharan</h4>
-                        <span>Designer</span>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
-                    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s"><span>My</span> SKILLS</h2>
             </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.6s">
-            <div class="team-wrapper">
-                <img src="images/member-1.jpg" class="img-responsive" alt="team img 2">
+            <div class="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.3s">
+                <div class="team-wrapper">
+                    <img src="{{ asset('images/front/hcj.webp') }}" class="img-responsive h-fx" alt="team img 1">
                     <div class="team-des">
-                        <h4>Giri Dharan</h4>
-                        <span>Developer</span>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
+                        <h4>Web Design</h4>
+                        <span>HTML & CSS & JS</span>
+                        <p class="p-skill">I have been designing web pages for five years using html css js, which makes me able to
+                            convert any psd or figma design into structured code and vibrant pages using javascript.</p>
                     </div>
+                </div>
             </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.3s">
-            <div class="team-wrapper">
-                <img src="images/member-1.jpg" class="img-responsive" alt="team img 3">
+            <div class="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.6s">
+                <div class="team-wrapper">
+                    <img src="{{ asset('images/front/laraphp.webp') }}" class="img-responsive h-fx" alt="team img 2">
                     <div class="team-des">
-                        <h4>Giri Dharan</h4>
-                        <span>Director</span>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
+                        <h4>Web Applcation Developer</h4>
+                        <span>PHP - LARAVEL </span>
+                        <p class="p-skill">In the field of web applications, I can build various applications such as e-commerce
+                            websites,
+                            school management systems, content management systems, and other systems according to
+                            the client's needs using the Laravel framework..</p>
                     </div>
+                </div>
             </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.6s">
-            <div class="team-wrapper">
-                <img src="images/member-1.jpg" class="img-responsive" alt="team img 4">
+            <div class="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.3s">
+                <div class="team-wrapper">
+                    <img src="{{ asset('images/front/flutter.webp ') }}" class="img-responsive h-fx" alt="team img 3">
                     <div class="team-des">
-                        <h4>Giri Dharan</h4>
-                        <span>Manager</span>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
+                        <h4>Mobile Developer </h4>
+                        <span>Flutter</span>
+                        <p class="p-skill">I have a year of experience in the field of building mobile applications for the Android
+                            system using Flutter and converting websites
+                            to Web View using Flutter. The course of developing my experience in this field.</p>
                     </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.6s">
+                <div class="team-wrapper">
+                    <img src="{{ asset('images/front/django.jpg') }}" class="img-responsive h-fx" alt="team img 4">
+                    <div class="team-des">
+                        <h4>Web Applcation Developer</h4>
+                        <span>PYTHON - DJANGO</span>
+                        <p class="p-skill">One of the most important frameworks that I can develop with is
+                            the Dejango framework, as I have two years of experience in developing web applications, and
+                            it is one of the most developed frameworks during the previous period..</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </section>
 <!-- end team -->
